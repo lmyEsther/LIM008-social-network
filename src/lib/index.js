2cbb33528e90270c3253ac997865a027c35ca154
lmyEsther <esthercheremo@gmail.com>
2019-01-25 18:53:32 -0500

    linkeado a proyecto de firebase mommylove

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,11 @@
 // aqui exportaras las funciones que necesites
 
-export const register = () => {
-  
-
-}
+// export const register = (email, password) => {
+//     firebase.auth().createUserWithEmailAndPassword(email, password)
+//     .catch(function(error) {
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         // ...
+//       });
+// }
