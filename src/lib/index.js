f235fdaad79ea609328699cf658d476b15ebcdd7
lmyEsther <esthercheremo@gmail.com>
2019-01-26 10:43:59 -0500

    cambios de html iniciar sesion de Yennifer

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,2 @@
 // aqui exportaras las funciones que necesites
 
-// export const register = (email, password) => {
-//     firebase.auth().createUserWithEmailAndPassword(email, password)
-//     .catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // ...
-//       });
-// }
