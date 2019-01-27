f99bdee05e68540d19fdf30be4a8e206e9a497ec
YferHurtado <yennifer.hurtadoarce@gmail.com>
2019-01-27 18:35:08 -0500

    mostrando en pantalla mensajes de error en el login y registro

--- src/lib/index.js
+++ src/lib/index.js
@@ -17,8 +17,6 @@ login.addEventListener('click', (ev) => {
       let errorCode = error.code;
       let errorMessage = error.message;
       // ...
-      console.log(errorCode);
-      console.log(errorMessage);
       document.getElementById("error-1").innerHTML = errorCode + " / " + errorMessage;
 
     });
