7b3ed3a7c018624533fc193009a3a4c72c78767c
Ivan <ivanlazo579@gmail.com>
2019-01-26 20:03:45 -0500

    Se agrego el archivo login-firebase para colocar las funciones para logear a google y registrarse por google, se agrego las reglas en el slint y se corrigio los errores de escritura en el main.js tambien se coloco un id en el html para los botones de google y se agrega la clase para los botones de registro

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@
 
 myFunction(); */
 
-const btnCuenta = document.getElementById("btn-crear-cuenta");
-btnCuenta.addEventListener("click", () => {
-    let nombre = document.getElementById("name").value;
-    let email = document.getElementById("correo").value;
-    let passw = document.getElementById("pass").value;
-    
-    firebase.auth().createUserWithEmailAndPassword(email, passw)
+const btnCuenta = document.getElementById('btn-crear-cuenta');
+btnCuenta.addEventListener('click', () => {
+// let nombre = document.getElementById('name').value;
+  let email = document.getElementById('correo').value;
+  let passw = document.getElementById('pass').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, passw)
     .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage)
-      });
-})
+    // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      // ...
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+});
